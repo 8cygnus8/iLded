77c5e5e823a4211f79f9d2480868c034e5fd1b6a
8cygnus8 <34145778+8cygnus8@users.noreply.github.com>
2017-12-08 18:32:09 -0600

    Update led i.ino

--- led i.ino
+++ led i.ino
@@ -1,17 +1,24 @@
-     void loop() // Prog_4_2
-          {
-               for ( int i = 6 ; i < 14 ; i++) // Definimos la variable i sobre la marcha
-                     {
-                          digitalWrite( i , HIGH) ;
-                          delay (500) ;
-                          digitalWrite( i , LOW);
-                          delay (500) ;
-                     }
-               for ( int i = 12 ; i >6 ; i--) // Definimos la variable i sobre la marcha
-                     {
-                         digitalWrite( i , HIGH) ;
-                         delay (500) ;
-                         digitalWrite( i , LOW);
-                         delay (500) ;
-                      }
-         }
+void setup()
+{ 
+  int i = 0 ;                        // Inicializamos la variable i como un entero
+  for ( i = 7 ;  i < 13 ; i++)
+      pinMode( i , OUTPUT) ;
+}
+void loop()
+  {
+    for ( int i = 7 ;  i < 13 ; i++)              // Definimos la variable i sobre la marcha
+      {
+          digitalWrite( i , HIGH) ;
+          delay (50) ;
+          digitalWrite( i , LOW);
+          delay (50) ;
+      }
+
+    for (  int i = 13 ;  i >7 ; i--)                  // Definimos la variable i sobre la marcha
+      {
+          digitalWrite( i , HIGH) ;
+          delay (50) ;
+          digitalWrite( i , LOW);
+          delay (50) ;
+      }
+
